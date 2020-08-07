9f7cbf45ed59743d5cbf2bd6b350d3faa379eb99
Kevin Zhang <56693266+kevin2hang@users.noreply.github.com>
2020-08-07 00:44:19 -0700

    Add ColorValue java class

---
+++ src/ColorValue.java
@@ -0,0 +1,31 @@
+public class ColorValue {
+    int r,g,b;
+
+    public int getG() {
+        return g;
+    }
+
+    public void setG(int g) {
+        this.g = g;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
+
+    public ColorValue(){
+
+    }
+}
